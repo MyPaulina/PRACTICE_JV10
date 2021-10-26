9318c04fa9eede37c36fbf4761a9b630fdc9c74c
PAULINA SALAZAR <83146423+MyPaulina@users.noreply.github.com>
2021-10-26 18:15:34 -0500

    Add files via upload

---
+++ 10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+ public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Introduce un nombre: ");
+        cadena = sc.nextLine();
+        System.out.println("Buenos Días " + cadena);                                               
+    }
+}
